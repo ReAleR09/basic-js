21507b51b176ec62e57d0c1dcd161e969ee5d1f0
Bogdan Z <realer09@gmail.com>
2020-03-18 23:29:17 +0300

    feat: implement solution for 'what-season' task

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,22 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+    if(arguments.length != 1) {
+        return 'Unable to determine the time of year!';
+    }
+    if(!(date instanceof Date) || Object.getOwnPropertyNames(date).length > 0) {
+        throw Error('What?!');
+    }
+    
+    const monthNum = date.getMonth() + 1;
+
+    switch(Math.floor(monthNum / 3)) {
+        case 4:
+        case 0:
+            return 'winter';
+        case 1: 
+            return 'spring';
+        case 2: 
+            return 'summer';
+        case 3:
+            return 'autumn';
+    }
 };

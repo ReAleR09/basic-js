d51c39dbc83fae82041dc04ea1b9d3c09e61c75b
Bogdan Z <realer09@gmail.com>
2020-03-19 21:11:15 +0300

    feat: implement solution for 'hanoi-tower' task

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,4 +1,9 @@
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+SECONDS_IN_HOUR = 3600;
+
+module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
+    const requiredTurns = Math.pow(2, disksNumber) - 1;
+    // time = turns / (turnsSpeed);
+    // initially turnsSpess is per hour, converting to per second
+    const requiredTime = requiredTurns / (turnsSpeed / SECONDS_IN_HOUR);
+    return {turns: requiredTurns, seconds: requiredTime};
 }

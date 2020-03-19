cb03e9b830cca88ef798622d711992c8b9287972
Bogdan Z <realer09@gmail.com>
2020-03-19 20:58:12 +0300

    feat: implement solution for task 'extended-repeater'

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,35 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+    if (options.hasOwnProperty('repeatTimes') && typeof options.repeatTimes !== 'number') {
+        options.repeatTimes = 1;
+    }
+    if (options.hasOwnProperty('additionRepeatTimes') && typeof options.additionRepeatTimes !== 'number') {
+        options.additionRepeatTimes = 1;
+    }
+    
+    if (!options.hasOwnProperty('separator')) {
+        options.separator = '+';
+    }
+
+    if (!options.hasOwnProperty('additionSeparator')) {
+        options.additionSeparator = '|';
+    }
+    if(typeof str !== 'string') {
+        str = '' + str;
+    }
+
+    let addition = '';
+    if(options.hasOwnProperty('addition') && options.additionRepeatTimes) {
+        addition = 
+            (new Array(options.additionRepeatTimes))
+                .fill('' + options.addition)
+                .join(options.additionSeparator);
+    }
+
+    const result = 
+        (new Array(options.repeatTimes))
+        .fill(str + addition)
+        .join(options.separator);
+
+    return result;
 };
   
